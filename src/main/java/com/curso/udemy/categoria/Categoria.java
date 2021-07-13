2a075fddb12efdad2885d67c14e1c8d299ff7808
Daniel <daniel.fecaf@outlook.com>
2021-07-13 13:28:49 -0300

    Organizando pacotes e criando Domínio e Repository

---
+++ src/main/java/com/curso/udemy/categoria/Categoria.java
@@ -0,0 +1,36 @@
+package com.curso.udemy.categoria;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+// SERIALIZABLE -> ESSA CLASSE, OS OBJETOS DELA PODEM SER CONVERTIDOS EM BYTES
+// OU SEJA, PARA QUE OS OBJETOS POSSAM SER GRAVADOS EM ARQUIVOS OU TRAFEGAR EM REDE
+
+@Entity
+public class Categoria implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String nome;
+
+	@Deprecated
+	public Categoria() {
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+}
