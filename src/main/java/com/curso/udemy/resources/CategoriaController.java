dd3b5e8069cff043f953d19d33938ba4906ac70e
Daniel <daniel.fecaf@outlook.com>
2021-07-13 13:13:31 -0300

    Criando o controller de categoria

---
+++ src/main/java/com/curso/udemy/resources/CategoriaController.java
@@ -0,0 +1,18 @@
+package com.curso.udemy.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaController {
+
+	
+	@GetMapping
+	public String Teste() {
+		return "Testado e funcionando!";
+	}
+
+	
+}
